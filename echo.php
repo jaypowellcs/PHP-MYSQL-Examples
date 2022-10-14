1362873931753e7e27b75dd79abdeeda07c1b06b
Jay Powell <jay.powellcs@gmail.com>
2022-10-14 15:20:05 -0500

    first commit

---
+++ echo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>echo Command</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2><?php echo 'Ivy\s' ; ?> page</h2>
+    <php echo '<p class="offer"> Offer: 20% off</p>' ?>
+</body>
+</html>
