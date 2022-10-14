0e61fc4e3a0cf931dee59ba14be6639b1f3dfb31
Jay Powell <jay.powellcs@gmail.com>
2022-10-14 15:24:58 -0500

    comment

---
+++ comment.php
@@ -0,0 +1,17 @@
+<?php 
+/* This page displays the member's name and details of the current offer */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding comments to your code</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2><?php echo 'Welcome Ivy'; //show name ?></h2>
+    <?php echo '<p class="offer"> Offer: 20% off</p>' ?>
+</body>
+</html>
